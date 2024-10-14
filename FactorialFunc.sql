f49a73950758bbbdd08ec828a276a0617d8d1b76
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-10-14 10:34:05 +0530

    Create FactorialFunc.sql

---
+++ FactorialFunc.sql
@@ -0,0 +1,27 @@
+set serveroutput on;
+show serveroutput;
+
+create or replace function factorial(n int)
+return int as fact int;
+
+begin
+  fact:=1;
+  for i in 1..n loop
+    fact:=fact * i;
+  end loop;
+  return fact;
+end;
+/
+
+--------------
+
+declare
+  c integer;
+  result integer;
+
+begin
+  c:=&c;
+  result:=factorial(c);
+  dbms_output.put_line('The factorial is '||result);
+end;
+/
