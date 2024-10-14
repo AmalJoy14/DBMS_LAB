bbdf1227b263759c38f37d69e02faaab74f84b75
Amal Joy <145443570+AmalJoy14@users.noreply.github.com>
2024-10-14 10:41:10 +0530

    Create TableManipulationFunc.sql

---
+++ TableManipulationFunc.sql
@@ -0,0 +1,21 @@
+--Display the total number of customers using function in pl/sql from the table customer.
+
+create or replace function totalcustomers
+return number as total number(2) := 0;
+
+begin
+   select count(*) into total from customers;
+   return total;
+end;
+/
+
+---------
+
+declare 
+   c number(2); 
+
+begin 
+   c := totalcustomers(); 
+   dbms_output.put_line('Total no. of Customers: ' || c); 
+end; 
+/
